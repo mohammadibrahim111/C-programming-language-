f8817a3a3e36abeae53e5d48e82038ce76735203
mohammadibrahim111 <mohammadibrahim@eng.rizvi.edu.in>
2025-12-05 04:06:01 +0530

    Create experiment 6.c

---
+++ experiment 6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void counter() {
+    static int count = 78; // static variable
+
+    count++;
+    printf("Count = %d\n", count);
+}
+
+int main() {
+    counter();
+    counter();
+    counter();
+
+    return 0;
+}
