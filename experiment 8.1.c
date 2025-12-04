ef4d948e6e53f3d51487ca0486be55e8888d6fb4
mohammadibrahim111 <mohammadibrahim@eng.rizvi.edu.in>
2025-12-05 04:13:36 +0530

    Create experiment 8.1.c

---
+++ experiment 8.1.c
@@ -0,0 +1,20 @@
+// Name: Mohammad Ibrahim
+// UIN: 251P111
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[i] != '\0') {
+        i++;
+    }
+
+    printf("Length of the string = %d", i);
+
+    return 0;
+}
