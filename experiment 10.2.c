b33a59c7e476586edc62c33ce37a0de7b48f3562
mohammadibrahim111 <mohammadibrahim@eng.rizvi.edu.in>
2025-12-05 14:43:59 +0530

    Create experiment 10.2.c

---
+++ experiment 10.2.c
@@ -0,0 +1,27 @@
+// Name: Mohammad Ibrahim
+// UIN : 251P111
+
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+    int *p;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    p = &a[n - 1];
+
+    printf("Array in reverse order:\n");
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", *p);
+        p--;
+    }
+
+    return 0;
+}
